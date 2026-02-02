6010ad9a411dab53630f3d3a981934a167d4597a
Colombine972 <decidela972@me.com>
2026-02-02 16:55:39 +0100

    create front

---
+++ client/src/pages/About.tsx
@@ -0,0 +1,6 @@
+function About () {
+    return <h1>Une nouvelle page about</h1>
+
+}
+
+export default About;
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,6 @@
+function home () {
+    return <h1>Nouvelle home</h1>
+
+}
+
+export default home;
