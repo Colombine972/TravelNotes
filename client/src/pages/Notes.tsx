146798226b2113a054dbb344e09d57cc53c1261e
Colombine972 <decidela972@me.com>
2026-02-08 08:29:50 +0100

    modified css

--- client/src/pages/Notes.tsx
+++ client/src/pages/Notes.tsx
@@ -5,7 +5,7 @@ type Note = {
     id: number;
     title: string;
     content: string;
-    created_at?: string;
+    created_at: string;
 };
 
 function Notes() {
@@ -41,7 +41,11 @@ return (
 
             <div className="note-content">
               <h3 className="note-title">{note.title}</h3>
-              <p className="note-create_at">{note.created_at}</p>
+              <p className="note-date"> {new Intl.DateTimeFormat("fr-FR", {
+              day: "2-digit",
+              month: "long",
+              year: "numeric",
+              }).format(new Date(note.created_at))}</p>
               <p className="note-text">{note.content}</p>
             </div>
           </article>
